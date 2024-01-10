7219658c7f0e57136dd301cf5b56f3cd601f91d7
Domenico Di Nardo <domenico.dnrd@hotmail.com>
2024-01-10 11:53:20 +0100

    add board init

--- apartments/serializers.py
+++ apartments/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Apartments
+from worksites.serializers import WorksiteSerializer
 
 class ApartmentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Apartments
+        fields = '__all__'
+
+
+
+
+class WorksiteApartmentsSerializer(serializers.ModelSerializer):
+    worksite = WorksiteSerializer()
 
     class Meta:
         model = Apartments
--- board/urls.py
+++ board/urls.py
@@ -5,6 +5,9 @@ app_name = 'board'
 urlpatterns = [
     path('board/', views.board_lists, name='board'),
     path('boards-lists-api/', views.board_list_api, name='board-lists-api'),
+
+    path('add-board/', views.add_board, name='add-board'),
+    path('add-new-board/', views.add_new_board, name='add-new-board'),
 ]
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
