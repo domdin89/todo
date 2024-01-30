bb7cc6d5fa1d5a08be5e312ce0b4b7d8b13e53b7
domdin <domenico.dnrd@hotmail.com>
2024-01-30 17:32:04 +0100

    caprover

--- falone/settings.py
+++ falone/settings.py
@@ -96,7 +96,7 @@ TEMPLATES = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
     'http://127.0.0.1:8000',
-    'https://falone.falone.madstudio.it/'
+    'https://falone.falone.madstudio.it'
 
 ]
 
