c2c38fec7fbbc72139c04151a2548444d412c410
domdin <domenico.dnrd@hotmail.com>
2024-02-14 11:43:00 +0100

    board recipient serializer update

--- board/serializers.py
+++ board/serializers.py
@@ -10,19 +10,30 @@ from .models import Boards, BoardsRecipient
 
 class RecipientsSerializer(serializers.ModelSerializer):
     worksites_id = serializers.PrimaryKeyRelatedField(
-        queryset=Worksites.objects.all(), write_only=True, source='worksites', required=False)
+        queryset=Worksites.objects.all(), write_only=True, required=False, allow_null=True, source='worksites')
     apartment_id = serializers.PrimaryKeyRelatedField(
-        queryset=Apartments.objects.all(), write_only=True, source='apartment', required=False)
+        queryset=Apartments.objects.all(), write_only=True, required=False, allow_null=True, source='apartment')
     profile_id = serializers.PrimaryKeyRelatedField(
-        queryset=Profile.objects.all(), write_only=True, source='profile', required=False)
-
-    worksites = WorksiteStandardSerializer(read_only=True, required=False, allow_null=True)
-    apartment = ApartmentSerializer(read_only=True, required=False, allow_null=True)
-    profile = ProfileSerializer(read_only=True, required=False, allow_null=True)
+        queryset=Profile.objects.all(), write_only=True, required=False, allow_null=True, source='profile')
 
     class Meta:
         model = BoardsRecipient
-        fields = ['id', 'date', 'recipient_type' ,'worksites_id', 'apartment_id', 'profile_id', 'worksites', 'apartment', 'profile']
+        fields = ['id', 'recipient_type', 'date', 'worksites_id', 'apartment_id', 'profile_id']
+
+    def create(self, validated_data):
+        # Validated data avrà i campi `worksites`, `apartment`, e `profile` direttamente grazie a `source`
+        return BoardsRecipient.objects.create(**validated_data)
+
+    def to_representation(self, instance):
+        """
+        Personalizza la rappresentazione in uscita per includere i dettagli completi
+        delle entità relazionate, se presenti.
+        """
+        representation = super().to_representation(instance)
+        representation['worksites'] = WorksiteStandardSerializer(instance.worksites).data if instance.worksites else None
+        representation['apartment'] = ApartmentSerializer(instance.apartment).data if instance.apartment else None
+        representation['profile'] = ProfileSerializer(instance.profile).data if instance.profile else None
+        return representation
 
 class BoardsSerializer(serializers.ModelSerializer):
     recipients = RecipientsSerializer(many=True)
@@ -32,10 +43,12 @@ class BoardsSerializer(serializers.ModelSerializer):
         fields = ['id', 'image', 'title', 'body', 'author', 'date', 'date_update', 'type', 'recipients']
 
     def create(self, validated_data):
+        # Estrai i dati dei destinatari (recipients) e rimuovili dai dati validati della board
         recipients_data = validated_data.pop('recipients', [])
+        # Crea l'istanza della board
         board = Boards.objects.create(**validated_data)
+        # Itera sui dati dei destinatari per creare le relative istanze di BoardsRecipient
         for recipient_data in recipients_data:
-            # Estrai i dati dei campi nidificati dopo la popolazione della chiave esterna 'board'
-            recipient_data = recipient_data.get('recipients') if 'recipients' in recipient_data else recipient_data
+            # Crea ogni istanza di BoardsRecipient associata alla board corrente
             BoardsRecipient.objects.create(board=board, **recipient_data)
         return board
