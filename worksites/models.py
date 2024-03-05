cc01e0b7c2e32f3b630d8e02e73b8069767a8c1e
cocco12892 <andrea@madstudiodesign.it>
2024-03-05 15:41:33 +0100

    migrations

--- worksites/models.py
+++ worksites/models.py
@@ -57,7 +57,7 @@ class Status(models.Model):
     order = models.IntegerField()
 
 class WorksitesStatus(models.Model):
-    #status = models.ForeignKey(Status, on_delete=models.CASCADE )
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
     worksite = models.ForeignKey(Worksites, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     date_update = models.DateTimeField(blank=True, null=True)
