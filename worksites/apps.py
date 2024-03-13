d9e34af58455ede074f65e6c0e81b9fd6c8d2ca6
domdin <domenico.dnrd@hotmail.com>
2024-03-13 12:01:11 +0100

    fix

--- worksites/apps.py
+++ worksites/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WorksitesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'worksites'
+
+    def ready(self):
+        import worksites.signals
