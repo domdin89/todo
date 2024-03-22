5850af1ee65b32b19cb94661eed19b8a30594c16
domdin <domenico.dnrd@hotmail.com>
2024-03-22 13:41:15 +0100

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('profiles', views.ProfileListCreateAPIView2.as_view()),
+    path('profiles1', views.ProfileListCreateAPIView2.as_view()),
 
     path('profiles2', views.ProfileListAPIView.as_view()),
     path('worksites2', views.WorksiteListAPIView.as_view()),
