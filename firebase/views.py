61f9913b8dfb6d72b038f1b48587781c30d6f7bd
cocco12892 <andrea@madstudiodesign.it>
2024-03-22 10:53:49 +0100

    changes and fixes

--- firebase/views.py
+++ firebase/views.py
@@ -95,9 +95,147 @@ def token_save(request):
 
     return Response({'message': 'Token salvato correttamente'}, status=status.HTTP_200_OK)
 
+def get_clienti_based_on_type(userId, notification_type):
+    print(notification_type)
+    firebases = Firebase.objects.filter(userId=userId)
+    
+    matching_firebases = []
+
+    for firebase in firebases:
+        permissions_data = firebase.permissions
+        print(permissions_data)
+    
+        if notification_type == 'Default':
+            matching_firebases.append(firebase)
+        elif notification_type == 'carichi' and permissions_data['carichi']:
+            matching_firebases.append(firebase)
+        elif notification_type == 'rifornimenti' and permissions_data['rifornimenti']:
+            matching_firebases.append(firebase)
+        elif 'types' in permissions_data:
+            print('entro qui')
+            type_numbers = {item['id'] for item in permissions_data['types']}
+            print('type_numbers',type_numbers)
+            print('notification_type.isdigit()',notification_type.isdigit())
+            if notification_type.isdigit() and int(notification_type) in type_numbers:
+                matching_firebases.append(firebase)
+
+    return matching_firebases
+
+def send_firebase_notification(request, userId, title, subtitle, body, badge, silent=None, data1=None, data2=None, path=None, type='Default'):
+
+    if silent:
+        title = ''
+        subtitle = ''
+        body = ''
+
+    Authorization = 'Bearer '+ _get_access_token()
+    Host = 'fcm.googleapis.com'
+    url = f"https://fcm.googleapis.com/v1/projects/{os.environ.get('FIREBASE_PROJECT_ID')}/messages:send"
+    
+    clienti = get_clienti_based_on_type(userId, type)
+
+    response = None
+
+    if not clienti:
+        print(f"No tokens found for userId: {userId}")
+        # Optionally, return some default response or raise an exception
+        return
+
+    for token in clienti:
+        token = token.token
+
+        headers = {
+            'Authorization': Authorization,
+            'Host': Host,
+            'Content-Type': 'application/json',
+        }
+
+        data = {
+            "message":{
+                "android": {
+                    "notification": {
+                        "image": "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
+                    }
+                },
+                "apns": {
+                    "headers": {
+                        "apns-priority": "5"
+                    },
+                    "payload": {
+                        "aps": {
+                            "badge" : badge,
+                            "sound" : {
+                                "critical": 1,
+                                "name":"bingbong.aiff",
+                                "volume": 1
+                            },
+                            "alert" : {
+                                "title" : title,
+                                "subtitle" : subtitle,
+                                "body" : body
+                            },
+                            "fcm_options": {
+                                "image": "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
+                            }
+                        }
+                    }
+                },
+                "token": token,
+                "data": {
+                    "path": path,
+                    "data1": data1,
+                    "data2": data2
+                }
+            }
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+    
+    return response
+
 @permission_classes([IsAdminUser]) 
 @api_view(['GET'])
+def firebase_push(request, userId):
+    title = request.query_params.get('title', os.environ.get('FIREBASE_APP_NAME'))
+    subtitle = request.query_params.get('subtitle', 'Subtitle')
+    body = request.query_params.get('body', 'Corpo della notifica')
+
+    silent = request.query_params.get('silent', None)
+    badge = int(request.query_params.get('badge', 0))
+
+    data1 = request.query_params.get('data1', None)
+    data2 = request.query_params.get('data2', None)
+    path = request.query_params.get('path', None)
+
+    response = send_firebase_notification(request, userId, title, subtitle, body, badge, silent, data1, data2, path, 'Default')
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/default_path/'))
+
+@permission_classes([AllowAny])
+@api_view(['GET'])
 def latest_app(request, platform):
-    version = LastVersion.objects.get(platform=platform)
-    serializer = LastVersionSerializer(version, many=False)
-    return Response(serializer.data)
+    base_url=os.environ.get('NOTIFICATION_SENDER_URL')
+    microservizio_url = f'{base_url}/latest_app/'
+    
+    api_key = os.environ.get('NOTIFICATION_SENDER_API_KEY')
+    
+    headers = {
+        'API-Key': f'{api_key}'
+    }
+    
+    response = requests.get(f'{microservizio_url}{platform}/', headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return Response(data)
+    else:
+        # Se la richiesta non ha avuto successo, restituisci un messaggio di errore
+        return Response({'error': 'Impossibile recuperare l\'ultima versione dell\'app dal microservizio'}, status=response.status_code)
+    
+def _get_access_token():
+    credentials = service_account.Credentials.from_service_account_file(
+        settings.GOOGLE_SERVICE_ACCOUNT_PATH, scopes=SCOPES)
+    
+    request = google_requests.Request()
+    credentials.refresh(request)
+    return credentials.token
