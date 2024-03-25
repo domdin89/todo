f1586ef2f3ab6303a1588941c7888c49dce83b26
domdin <domenico.dnrd@hotmail.com>
2024-03-25 17:53:24 +0100

    fix

--- apartments/serializers.py
+++ apartments/serializers.py
@@ -29,7 +29,7 @@ class ApartmentBaseSerializer(serializers.ModelSerializer):
         # Ottieni il worksite dal contesto
         
         # Filtra i CollabWorksites che sono validi e appartengono al worksite specifico
-        valid_subs = obj.collabworksites.filter(subs__is_valid=True, subs__apartment_id=obj.id)
+        valid_subs = obj.subs.filter(subs__is_valid=True, subs__apartment_id=obj.id)
         
         return ApartmentSubSerializer(valid_subs, many=True).data 
 
