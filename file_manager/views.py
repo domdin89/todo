b110fcc51cf47ef5fd05e590c402dbbd306deb25
domdin <domenico.dnrd@hotmail.com>
2024-03-27 11:48:37 +0100

    fix

--- file_manager/views.py
+++ file_manager/views.py
@@ -1,9 +1,13 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Directory
+from .models import Directory, File
 from rest_framework.permissions import IsAdminUser
-from .serializers import DirectorySerializerChildren  
+from .serializers import DirectorySerializerChildren
+import boto3
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from django.conf import settings
 
 @api_view(['GET'])
 #@permission_classes([IsAdminUser])
@@ -28,3 +32,24 @@ def get_directories(request):
 
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+def get_file_path(file_id):
+    try:
+        file = File.objects.get(id=file_id)
+        return file.file
+    except File.DoesNotExist:
+        return None
+    
+@api_view(['GET'])
+#@permission_classes([IsAdminUser])
+def get_file(request, file_id):
+    # Assumi di avere una funzione che recupera il percorso del file su S3 in base a file_id
+    file_path = get_file_path(file_id)
+    
+    # Crea un client S3
+    s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY, region_name=settings.AWS_S3_REGION_NAME)
+    
+    # Genera l'URL firmato
+    signed_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': file_path}, ExpiresIn=3600)  # URL valido per 1 ora
+    
+    return JsonResponse({'url': signed_url})
