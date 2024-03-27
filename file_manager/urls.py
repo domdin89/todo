a1839e3eb216071a7bd0e90c4fbb9789b373aa48
domdin <domenico.dnrd@hotmail.com>
2024-03-27 11:49:38 +0100

    fix

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_directories
+from .views import get_directories, get_file
 
 urlpatterns = [
     # Altre URL della tua app...
     path('directories/', get_directories, name='get_directories'),
+    path('get_file', get_file, name='get_file'),
     # Eventuali altre URL...
 ]
