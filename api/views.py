6c2a76a556b5f162e1aa5f58e3b03c06e75df268
Domenico Di Nardo <domenico.dnrd@hotmail.com>
2024-04-15 17:34:18 +0200

    fix

--- api/views.py
+++ api/views.py
@@ -182,7 +182,7 @@ def apartment_code_validator(request):
 
         jwt_token, access_token = login_without_password(access_code.profile) # type: ignore
 
-        profile = Profile.objects.get(id=access_code.profile.id)
+        profile = Profile.objects.get(id=access_code.profile)
         serializer = ProfileSerializer(profile)
 
         # if str(access_token) and str(jwt_token):
