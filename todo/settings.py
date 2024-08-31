a7e9e3f4840c13de79c494659f6aa59b61b80d1d
Domenico Di Nardo <domenico@MacBook-Pro-di-Domenico.local>
2024-08-31 12:08:42 +0200

    fix

--- todo/settings.py
+++ todo/settings.py
@@ -53,15 +53,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_crontab',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'wkhtmltopdf',
-    'fattura',
-    'settings',
-    'accounts'
+    'accounts',
+    'api'
 ]
 
 WKHTMLTOPDF_CMD_OPTIONS = {
